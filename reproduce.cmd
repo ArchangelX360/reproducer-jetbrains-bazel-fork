acf7e680481e04037eb03f1594442ddb38f05d51
Titouan Bion <titouan.bion@jetbrains.com>
2026-05-05 13:04:53 +0200

    Fix deletion command

--- reproduce.cmd
+++ reproduce.cmd
@@ -1,5 +1,4 @@
-remove-item -recurse -force C:\ProgramData\reproducer\out
-
+rmdir /s /q C:\ProgramData\reproducer\out
 bazelisk.exe build //:consumer
 bazelisk.exe clean
 bazelisk.exe build //:consumer
