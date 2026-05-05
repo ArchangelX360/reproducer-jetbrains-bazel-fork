a94be72da7b28ba3a61eb82bbee2b3cd6549e6ba
Titouan Bion <titouan.bion@jetbrains.com>
2026-05-05 14:13:16 +0200

    Prevent always failing on bazel out exsting

--- reproduce.cmd
+++ reproduce.cmd
@@ -1,5 +1,4 @@
-bazelisk.exe shutdown
-if exist "C:\ProgramData\reproducer\out" exit 1
+if exist "C:\ProgramData\reproducer\out" bazelisk.exe shutdown && exit 1
 bazelisk.exe clean --expunge
 bazelisk.exe build //:consumer --disk_cache=C:/ProgramData/reproducer/out/dc
 bazelisk.exe clean
