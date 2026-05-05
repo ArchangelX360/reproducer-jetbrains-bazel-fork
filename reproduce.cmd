58cd14f658d7a85c869a814005c84122a7fdb701
Titouan Bion <titouan.bion@jetbrains.com>
2026-05-05 14:07:29 +0200

    Fix disk cache location 2

--- reproduce.cmd
+++ reproduce.cmd
@@ -1,5 +1,5 @@
-move C:\ProgramData\reproducer\out C:\ProgramData\reproducer\out_bak
 bazelisk.exe clean --expunge
-bazelisk.exe build //:consumer
+if exist "C:\ProgramData\reproducer\out" exit 1
+bazelisk.exe build //:consumer --disk_cache=C:/ProgramData/reproducer/out/dc
 bazelisk.exe clean
-bazelisk.exe build //:consumer
+bazelisk.exe build //:consumer --disk_cache=C:/ProgramData/reproducer/out/dc
