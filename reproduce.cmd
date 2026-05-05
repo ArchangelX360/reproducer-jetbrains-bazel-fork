2dd6a0a2475f814002483aa6f2ae44481b7355f4
Titouan Bion <titouan.bion@jetbrains.com>
2026-05-05 14:09:10 +0200

    Fix fail if dir exists

--- reproduce.cmd
+++ reproduce.cmd
@@ -1,5 +1,5 @@
-bazelisk.exe clean --expunge
 if exist "C:\ProgramData\reproducer\out" exit 1
+bazelisk.exe clean --expunge
 bazelisk.exe build //:consumer --disk_cache=C:/ProgramData/reproducer/out/dc
 bazelisk.exe clean
 bazelisk.exe build //:consumer --disk_cache=C:/ProgramData/reproducer/out/dc
